38c5fe781a6b59fa520ca6eb3e3017e6e6192d73
py76dmr <py76dmr.@eduvaud.ch>
2025-01-10 15:47:14 +0100

    Dames14

--- Dames_main.py
+++ Dames_main.py
@@ -1,7 +1,7 @@
 # Dames_main.py
 import pygame
 from Dames_gfx import dessiner_plateau
-from Dames_bkend import creer_plateau, mouvement_valide, capture_valide, effectuer_mouvement, peut_continuer_capture
+from Dames_bkend import creer_plateau, mouvement_valide, capture_valide, effectuer_mouvement, peut_continuer_capture, verifier_coups_possibles
 from Dames_menu import afficher_menu_final
 
 pygame.init()
@@ -58,73 +58,29 @@ def capture_obligatoire(joueur, plateau):
                 return True
     return False
 
-def capture_possible(joueur, plateau):
-    for ligne in range(hauteur):
-        for colonne in range(longueur):
-            piece = plateau[ligne][colonne]
-            if piece and piece[0] == joueur:
-                if peut_continuer_capture(ligne, colonne, plateau):
-                    return True
-    return False
-
 while en_cours:
     for evenement in pygame.event.get():
         if evenement.type == pygame.QUIT:
             en_cours = False
 
-        if evenement.type == pygame.KEYDOWN:
-            if evenement.key == pygame.K_BACKSPACE and len(historique) > 1:
-                historique.pop()
-                plateau = [ligne[:] for ligne in historique[-1]]
-                joueur_actuel = "B" if joueur_actuel == "R" else "R"
-                piece_selectionnee = None
-
         if evenement.type == pygame.MOUSEBUTTONDOWN:
             colonne, ligne = evenement.pos[0] // TAILLE_CASE, evenement.pos[1] // TAILLE_CASE
             if piece_selectionnee:
                 depart_ligne, depart_colonne = piece_selectionnee
-                if capture_valide(depart_ligne, depart_colonne, ligne, colonne, plateau):
+                if capture_valide(depart_ligne, depart_colonne, ligne, colonne, plateau) or mouvement_valide(depart_ligne, depart_colonne, ligne, colonne, plateau):
                     historique.append([ligne[:] for ligne in plateau])
-                    nouveau_joueur = effectuer_mouvement(depart_ligne, depart_colonne, ligne, colonne, plateau)
-                    if nouveau_joueur:
-                        joueur_actuel = nouveau_joueur
-                        piece_selectionnee = None
-                        if joueur_actuel == "R":
-                            coups_rouge += 1
-                        else:
-                            coups_bleu += 1
-                    else:
-                        piece_selectionnee = (ligne, colonne)
-                elif mouvement_valide(depart_ligne, depart_colonne, ligne, colonne, plateau) and not capture_possible(joueur_actuel, plateau):
-                    historique.append([ligne[:] for ligne in plateau])
-                    nouveau_joueur = effectuer_mouvement(depart_ligne, depart_colonne, ligne, colonne, plateau)
-                    if nouveau_joueur:
-                        joueur_actuel = nouveau_joueur
-                        piece_selectionnee = None
-                        if joueur_actuel == "R":
-                            coups_rouge += 1
-                        else:
-                            coups_bleu += 1
-                    else:
-                        piece_selectionnee = (ligne, colonne)
-                else:
+                    joueur_actuel = effectuer_mouvement(depart_ligne, depart_colonne, ligne, colonne, plateau)
                     piece_selectionnee = None
-            elif plateau[ligne][colonne] and plateau[ligne][colonne][0] == joueur_actuel:
-                if capture_possible(joueur_actuel, plateau):
-                    if peut_continuer_capture(ligne, colonne, plateau):
-                        piece_selectionnee = (ligne, colonne)
                 else:
                     piece_selectionnee = (ligne, colonne)
+            elif plateau[ligne][colonne] and plateau[ligne][colonne][0] == joueur_actuel:
+                piece_selectionnee = (ligne, colonne)
 
-    dessiner_plateau(ecran, plateau, piece_selectionnee, TAILLE_CASE, longueur, hauteur)
-
-    # Vérifier si un joueur a gagnу
-    gagnant = verifier_victoire(plateau)
-    if gagnant:
-        # Afficher le menu final
+    # Перевірка чи є можливі ходи
+    if not verifier_coups_possibles(joueur_actuel, plateau):
+        gagnant = "Bleu" if joueur_actuel == "R" else "Rouge"
         choix = afficher_menu_final(ecran, gagnant, coups_rouge, coups_bleu, LARGEUR, HAUTEUR)
         if choix == "restart":
-            # Recommencer la partie
             plateau = creer_plateau(longueur, hauteur)
             historique = [creer_plateau(longueur, hauteur)]
             joueur_actuel = "R"
@@ -132,8 +88,9 @@ while en_cours:
             coups_bleu = 0
             piece_selectionnee = None
         else:
-            en_cours = False  # Quitter le jeu
+            en_cours = False
 
+    dessiner_plateau(ecran, plateau, piece_selectionnee, TAILLE_CASE, longueur, hauteur)
     pygame.display.flip()
 
 pygame.quit()
